4edd933b6c2553333f7358399c41cf014cfb3e29
JulianoSDev <Julianosavio32@gmail.com>
2025-03-15 11:23:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ function showMessage() {
     document.getElementById("messageBox").classList.add("show");
     document.getElementById("pageBody").style.backgroundColor = "#ffccdd"; // Fundo rosa claro
     document.getElementById("gif").style.display = "block"; // Exibe o GIF
+
+    
+    setTimeout(() => {
+        document.querySelector(".tiktok-button").style.display = "block";
+    },  25300); 
 }

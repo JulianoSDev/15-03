01aa9aadee33c70ff48d200d921a1e20e1b7fc1e
JulianoSDev <Julianosavio32@gmail.com>
2025-03-15 11:01:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    document.getElementById("messageBox").classList.add("show");
+    document.getElementById("pageBody").style.backgroundColor = "#ffccdd"; // Fundo rosa claro
+    document.getElementById("gif").style.display = "block"; // Exibe o GIF
+}
